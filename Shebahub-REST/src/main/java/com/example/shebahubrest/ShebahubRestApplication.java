57780b405e779abec37433f5568b95c4206b44ef
ashenafiteame <ashenafitm@gmail.com>
2022-09-25 12:41:19 -0500

    port added

--- Shebahub-REST/src/main/java/com/example/shebahubrest/ShebahubRestApplication.java
+++ Shebahub-REST/src/main/java/com/example/shebahubrest/ShebahubRestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShebahubRestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShebahubRestApplication.class, args);
 	}
