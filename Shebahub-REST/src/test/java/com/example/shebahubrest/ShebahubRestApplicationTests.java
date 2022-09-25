468f9a5a0eeccccd5ed5a9d90dc3f8a64fe86ab6
ashenafiteame <ashenafitm@gmail.com>
2022-09-25 12:18:46 -0500

    shebahub Rest added

---
+++ Shebahub-REST/src/test/java/com/example/shebahubrest/ShebahubRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shebahubrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShebahubRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
